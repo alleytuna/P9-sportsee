f1ebe1d3ed4179c620bd16ddb9b2ec1fbe865bb3
alleytuna <anais.altun@gmail.com>
2023-02-14 15:05:06 +0100

    cleanup, lint and packages

--- sportsee-front/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return(
-        <main>
-            <p>Bonjour</p>
-            <p> Félicitations ! Vous avez explosé vos objectifs d'hier</p>
-            
-        </main>
-    )
-}
